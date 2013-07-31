b832ac7f1f02c0b7110fa825ab9e6e53ca2997e8
Ravindra Ranwala <ravindraranwala@gmail.com>
2013-07-31 21:11:26 +0530

    Fetched data from Nasa Data Feed uri

--- Assignment/src/com/msc/servlet/SearchServlet.java
+++ Assignment/src/com/msc/servlet/SearchServlet.java
@@ -1,6 +1,11 @@
 package com.msc.servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -24,6 +29,26 @@ public class SearchServlet extends HttpServlet {
 		// TODO: Fill the Business logic here.
 		String query = req.getParameter("query");
 		log.info(query);
-	}
+		BufferedReader reader = null;
+		List<String> resultList = null;
 
+		// Send the query to the nasa data and fetch the results back.
+		try {
+			URL url = new URL(
+					"http://data.nasa.gov/api/get_search_results/?search=saturn");
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+			String line = null;
+			resultList = new ArrayList<String>();
+
+			while ((line = reader.readLine()) != null) {
+				// Add the read line into the result list.
+				resultList.add(line + "\n");
+				log.info(line + "\n");
+			}
+		} finally {
+			reader.close();
+		}
+
+	}
 }
