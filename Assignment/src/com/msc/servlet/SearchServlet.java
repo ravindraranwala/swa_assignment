8b482c1fba39f594d58347568ddc0e66f4870a49
Ravindra Ranwala <ravindraranwala@gmail.com>
2013-08-03 11:36:51 +0530

    Storage functionality was introduced

--- Assignment/src/com/msc/servlet/SearchServlet.java
+++ Assignment/src/com/msc/servlet/SearchServlet.java
@@ -2,6 +2,7 @@ package com.msc.servlet;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -14,6 +15,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Text;
 import com.msc.json.JsonReader;
 import com.msc.model.SearchResult;
 
@@ -30,7 +39,6 @@ public class SearchServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		StringBuffer searchResult = new StringBuffer("");
 		List<SearchResult> searchResults = new ArrayList<SearchResult>();
 
 		try {
@@ -51,8 +59,8 @@ public class SearchServlet extends HttpServlet {
 				String content = jo1.get("content").toString();
 				String title = jo1.getString("title_plain").toString();
 				searchResults.add(new SearchResult(title, content));
-				log.info("Title: " + title);
-				log.info("Content: " + content);
+				// log.info("Title: " + title);
+				// log.info("Content: " + content);
 			}
 
 		} catch (JSONException e) {
@@ -61,6 +69,7 @@ public class SearchServlet extends HttpServlet {
 					e);
 		}
 
+		saveLatestResults(searchResults);
 		// Modified the code to use tag libraries instead of jsp scriptlets
 		// here.
 		req.setAttribute("searchResult", searchResults);
@@ -68,5 +77,34 @@ public class SearchServlet extends HttpServlet {
 
 		// We are NOT using jsp scriptlets anymore.
 		// resp.sendRedirect("/search.jsp?searchResult=" + searchResult);
+	}
+
+	private void saveLatestResults(List<SearchResult> searchResults) {
+		Key searchKey = KeyFactory.createKey("SearchResult", "search");
+		Entity resultEntity = null;
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+
+		// Add the latests search results here.
+		for (SearchResult searchResult : searchResults) {
+			resultEntity = new Entity("SearchResult", searchKey);
+			resultEntity.setProperty("title", searchResult.getTitle());
+			resultEntity.setProperty("content",
+					new Text(searchResult.getContent()));
+			resultEntity.setProperty("date", new Date());
+
+			datastore.put(resultEntity);
+		}
+
+		// Remove the stale/obsoleted search results here.
+		Query query = new Query("SearchResult", searchKey).addSort("date",
+				Query.SortDirection.DESCENDING);
+		List<Entity> results = datastore.prepare(query).asList(
+				FetchOptions.Builder.withLimit(100));
+		for (Entity entity : results) {
+			log.info("Title: " + entity.getProperty("title") + " Content: "
+					+ entity.getProperty("content") + " Date: "
+					+ entity.getProperty("date"));
+		}
 	}
 }
